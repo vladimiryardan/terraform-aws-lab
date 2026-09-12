5963587c446a8f7cb086a1029ea48b4a42f3a244
Vladimir Yardan <vladimiryardan@gmail.com>
2026-09-12 22:42:09 +0800

    Add public route table and internet route

--- main.tf
+++ main.tf
@@ -44,3 +44,25 @@ resource "aws_internet_gateway" "lab_igw" {
     ManagedBy   = "Terraform"
   }
 }
+
+#aws_route_table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name        = "terraform-lab-public-rt"
+    Environment = "training"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.lab_igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
